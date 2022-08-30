bbdbc1c4b6a6984970ce881b07ad51838d05db4a
abarekatain <alireza.barekatain1996@gmail.com>
2022-08-30 17:01:04 +0200

    Add live control trajectory recording capability

--- include/lfd_interface/lfd_controller.h
+++ include/lfd_interface/lfd_controller.h
@@ -14,8 +14,7 @@
 
 #include <lfd_interface/moveit_util.h>
 
-#include <moveit_msgs/DisplayTrajectory.h>
-#include <moveit_msgs/RobotTrajectory.h>
+#include <trajectory_msgs/JointTrajectory.h>
 
 class LFDController
 {
@@ -28,10 +27,17 @@ private:
 
     actionlib::SimpleActionClient<lfd_interface::ControlLFDAction> ac_control_;
 
+    bool STOP_RECORDING = false;
+
+    trajectory_msgs::JointTrajectory recorded_traj_;
+
+    ros::Rate loop_rate;
 
 public:
     LFDController(MoveitUtil & moveit_util);
     ~LFDController();
     void runControl(lfd_interface::PlanMsg plan_metadata);
+
+    // void recordMovement();
 };
 
--- src/lfd_interface/lfd_controller.cpp
+++ src/lfd_interface/lfd_controller.cpp
@@ -1,9 +1,10 @@
 #include <lfd_interface/lfd_controller.h>
 
 LFDController::LFDController(MoveitUtil & moveit_util):
-moveit_util_(moveit_util), ac_control_("control_lfd", true)
+moveit_util_(moveit_util), ac_control_("control_lfd", true), loop_rate(20)
 {
-
+    recorded_traj_.joint_names = moveit_util_.getMoveGroup()->getJointNames();
+    
 }
 
 LFDController::~LFDController() {}
@@ -13,18 +14,33 @@ void LFDController::runControl(lfd_interface::PlanMsg plan_metadata)
     
     ac_control_.waitForServer();
     ROS_INFO_NAMED(LOGNAME, "Control action server ready");
-
     lfd_interface::ControlLFDGoal goal;
     goal.plan = plan_metadata;
     ac_control_.sendGoal(goal);
 
-    bool finished_before_timeout = ac_control_.waitForResult(ros::Duration(30.0));
+    recorded_traj_.points.clear();
+    trajectory_msgs::JointTrajectoryPoint current_state;
 
-    if(finished_before_timeout)
+    while(ros::ok())
     {
+        loop_rate.sleep();
         actionlib::SimpleClientGoalState state = ac_control_.getState();
-        ROS_INFO_NAMED(LOGNAME, "Control Action finished: %s",state.toString().c_str());
+
+        if (state == actionlib::SimpleClientGoalState::ACTIVE ||
+            state == actionlib::SimpleClientGoalState::PENDING)
+        {
+            moveit_util_.currentJointState(current_state);
+            recorded_traj_.points.push_back(current_state);
+        }
+        else break;
     }
-    else
-        ROS_INFO_NAMED(LOGNAME, "Action did not finish before the time out.");
+
+    ROS_INFO("Showing recorded trajectory in Red");
+    moveit_util_.changeVisualizationColor(rviz_visual_tools::RED);
+    moveit_util_.visualizeJointTrajectory(recorded_traj_);
+
+
+    actionlib::SimpleClientGoalState state = ac_control_.getState();
+    ROS_INFO_NAMED(LOGNAME, "Control Action finished: %s",state.toString().c_str());
+
 }
