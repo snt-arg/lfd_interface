c462ae5db343e6d9f737add4569d59a7d120d7b8
abarekatain <alireza.barekatain1996@gmail.com>
2023-11-07 10:40:55 +0100

    feat: normalize angle

--- src/lfd_camera/calib.py
+++ src/lfd_camera/calib.py
@@ -1,6 +1,7 @@
 
 import rospy
 import numpy as np
+import math
 
 from geometry_msgs.msg import Pose
 from tf.transformations import quaternion_from_euler, quaternion_multiply
@@ -28,7 +29,7 @@ class Calibration:
     def _fill_pose_template(self,pose_template: Pose, p):
         pose_template.position.x = p[0]
         pose_template.position.y = p[1]
-        pose = self._rotate_pose_around_z(pose_template,p[2])
+        pose = self._rotate_pose_around_z(pose_template,-p[2])
         return pose
     
     def _rotate_pose_around_z(self,pose, x):
@@ -42,7 +43,7 @@ class Calibration:
         Returns:
         Pose: The rotated Pose message.
         """
-        
+        x = self._normalize_angle(x)
         orientation = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
         rotation_quaternion = quaternion_from_euler(0, 0, x)
         rotated_orientation = quaternion_multiply(orientation, rotation_quaternion)
@@ -55,4 +56,16 @@ class Calibration:
         rotated_pose.orientation.w = rotated_orientation[3]
         
         return rotated_pose
+    
+    def _normalize_angle(self, angle):
+        """
+        angle: radians
+        """
+        max_rotation_angle = math.radians(90)
+        angle = math.atan2(math.sin(angle), math.cos(angle))
+        if abs(angle) > max_rotation_angle:
+            angle = angle + math.radians(180)
+            angle = math.atan2(math.sin(angle), math.cos(angle))
+        return angle
+
     
