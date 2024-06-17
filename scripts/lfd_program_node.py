fdae34bb4d0d027d27093c534c335cfcc186e552
abarekatain <alireza.barekatain1996@gmail.com>
2024-06-17 11:46:15 +0200

    feat: add launcher for program node

--- scripts/lfd_program_node.py
+++ scripts/lfd_program_node.py
@@ -9,16 +9,24 @@ if __name__ == "__main__":
 
     rospy.init_node("lfd_program_node", anonymous=False)
 
-    robot = "yumi_l"
-    duration_scale = 1
-    camera = True
+    robot = rospy.get_param("~robot", "yumi_l")
+    duration_scale = rospy.get_param("~duration_scale", 5)
+    camera = rospy.get_param("~camera", False)
 
     if robot=="yumi_r":
         runner = ProgramRunner(robot_type="yumi_r", camera=camera, duration_scale=duration_scale)
         runner.move("nomatter", "smoothyrtest")
     elif robot=="yumi_l":
         runner = ProgramRunner(robot_type="yumi_l", camera=camera, duration_scale=duration_scale)
-        runner.move("screw", "smoothylhometoscrew")
+
+        # runner.gripper("grasp", "close")
+
+        # runner.move("screw", "smoothylhometoscrew")
+        # runner.move(None, "smoothylscrewmount")
+        # runner.move("ring", "smoothylscrewtoring")
+
+        # runner.move("ring", "smoothylringtest")
+
     elif robot=="fr3":
         runner = ProgramRunner(camera=camera, duration_scale=duration_scale)
         runner._dmp_train("smoothfrpick")
